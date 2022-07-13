3857c48b5edd0d31de6b019432bba3141f5cd1a1
Jhoell Opeyemi <75471937+JhoellOpeyemi@users.noreply.github.com>
2022-07-13 09:54:12 +0100

    fixed cart bug

--- src/contexts/ProductsContext.js
+++ src/contexts/ProductsContext.js
@@ -11,8 +11,12 @@ export const ProductsProvider = ({ children }) => {
   const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart"));
   const totalPriceFromLocalStorage = JSON.parse(localStorage.getItem("total"));
 
-  if (localStorage.getItem("cart") == null) {
+  if (
+    localStorage.getItem("cart") == null ||
+    localStorage.getItem("total") == null
+  ) {
     localStorage.setItem("cart", "[]");
+    localStorage.setItem("total", "[]");
   }
 
   const [collections, setCollections] = useState({});
