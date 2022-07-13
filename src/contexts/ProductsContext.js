6317608857cd45f7e2e3e620da066415b0371351
Jhoell Opeyemi <75471937+JhoellOpeyemi@users.noreply.github.com>
2022-07-13 12:40:33 +0100

    fixed local storage bug

--- src/contexts/ProductsContext.js
+++ src/contexts/ProductsContext.js
@@ -1,16 +1,42 @@
 import { useState, useEffect, createContext } from "react";
 
+// creating a context so the states, functions and logic in this file will be available to other components without passing props from parent components
 export const ProductsContext = createContext();
 
+// GET ITEMS FROM LOCAL STORAGE
+// a variable that holds the value of 'productOverview' from local storage
+const productOverviewFromStorage = JSON.parse(
+  localStorage.getItem("productOverview") || "{}"
+);
+
+// a variable that holds the value of 'cart' from local storage
+const cartFromStorage = JSON.parse(localStorage.getItem("cart") || "[]");
+
+// a variable that holds the value of 'totalPrice' from local storage
+const totalPriceFromStorage = JSON.parse(localStorage.getItem("total") || "[]");
+
 export const ProductsProvider = ({ children }) => {
+  // a state to hold products data
   const [products, setProducts] = useState({});
 
+  // a state to hold collections data
   const [collections, setCollections] = useState({});
-  const [productOverview, setProductOverview] = useState({});
-  const [cart, setCart] = useState([]);
-  const [totalPrice, setTotalPrice] = useState([]);
+
+  // a state to hold product overview data after a product is clicked. if the page is refreshed, the state is set to the data from local storage
+  const [productOverview, setProductOverview] = useState(
+    productOverviewFromStorage
+  );
+
+  // a state to hold cart data. if the page is refreshed, the state is set to the data from local storage
+  const [cart, setCart] = useState(cartFromStorage);
+
+  // a state to hold and manage the total price of items in the cart, type of this state is array. if the page is refreshed, the state is set to the data from local storage
+  const [totalPrice, setTotalPrice] = useState(totalPriceFromStorage);
+
+  // a state to hold the sum of all cart item prices. this state will be updated using totalPrice.reduce array method
   const [subTotal, setSubTotal] = useState();
 
+  // the base url of my api endpoint for retrieving data from strapi
   const baseUrl = "https://dhrey-store.herokuapp.com/api";
 
   const fetchCategories = () => {
@@ -19,6 +45,7 @@ export const ProductsProvider = ({ children }) => {
       .then((data) => setCollections(data));
   };
 
+  // a function to filter the products based on the category passed in and update the products state
   const filterProducts = (category) => {
     setProducts({});
     fetch(
@@ -67,6 +94,13 @@ export const ProductsProvider = ({ children }) => {
   useEffect(() => {
     fetchCategories();
   }, []);
+
+  // useEffect for setting data to local storage
+  useEffect(() => {
+    localStorage.setItem("productOverview", JSON.stringify(productOverview));
+    localStorage.setItem("cart", JSON.stringify(cart));
+    localStorage.setItem("total", JSON.stringify(totalPrice));
+  }, [productOverview, cart, totalPrice]);
 
   return (
     <ProductsContext.Provider
